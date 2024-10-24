38bb937da1d275d0fe2b5b623ec85cfe1b594e34
MrR736 <mrr6451@outlook.com>
2024-10-24 18:59:47 -0400

    Add files via upload

--- Package/Remove.sh
+++ Package/Remove.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-echo "[https://github.com/MrR736]"
-
 COMMAND_NAME="Script Package Tool"
 SCRIPTS_BIN="/usr/bin/spt"
 SCRIPTS_FOLDER="/usr/scripts/ScriptPackageTool"
 
-if [ "$(whoami)" != "root" ]
-then
-  echo "You Have To Run As Superuser!"
-  exit 1
+if [ "$(whoami)" != "root" ]; then
+    echo -e "\e[31mE: You have to run as Superuser\e[0m"
+    exit 1
 fi
 
 if [[ ! -e "$SCRIPTS_BIN" ]]; then
     echo "$COMMAND_NAME is Not Install, so Cancelling Removed."
     exit 1
 else
-    echo "Remove $COMMAND_NAME"
-    rm -rf "$SCRIPTS_BIN" "$SCRIPTS_FOLDER"
+    if ! command -v rm &> /dev/null; then
+        echo -e "\e[31mE: rm Is Not Installed.\e[0m"
+        exit 1
+    else
+        echo "Remove $COMMAND_NAME..."
+        rm -rf "$SCRIPTS_BIN" "$SCRIPTS_FOLDER"
+    fi
 fi
